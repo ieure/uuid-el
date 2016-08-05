913d44d73b7e5a290b656e29e6e9512603b3776a
Ian Eure <ian.eure@gmail.com>
2016-08-05 15:34:49 -0700

    Highlight by UUID type.
    Highlight invalid UUIDs.
    
    Disable mode lighter.
    
    Rename some functions.
    
    Fix uuid-save-uuid-at-point.

--- uuid.el
+++ uuid.el
@@ -1,6 +1,6 @@
 ;;; uuid.el --- Minor mode for highlighting UUIDs    -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014 Ian Eure
+;; Copyright (C) 2014, 2016 Ian Eure
 
 ;; Author: Ian Eure <ian.eure@gmail.com>
 ;; Keywords: convenience
@@ -24,55 +24,143 @@
 
 ;;; Code:
 
-(defconst uuid-re
-  "\\b[0-9a-f]\\{8\\}-[0-9a-f]\\{4\\}-[0-9a-f]\\{4\\}-[0-9a-f]\\{4\\}-[0-9a-f]\\{12\\}\\b")
+(defconst uuid-anyhex "[0-9a-fA-F]")
+
+(defconst uuid-unknown-type-re
+  (concat "\\b" uuid-anyhex "\\{8\\}-" uuid-anyhex "\\{4\\}-"
+          uuid-anyhex "\\{4\\}-" uuid-anyhex "\\{4\\}-" uuid-anyhex
+          "\\{12\\}\\b"))
+
+(defconst uuid-type1-re
+  (concat "\\b" uuid-anyhex "\\{8\\}-" uuid-anyhex "\\{4\\}-1" uuid-anyhex "\\{3\\}-"
+          uuid-anyhex "\\{4\\}-" uuid-anyhex "\\{12\\}\\b"))
+
+(defconst uuid-type2-re
+  (concat "\\b" uuid-anyhex "\\{8\\}-" uuid-anyhex "\\{4\\}-2"
+          uuid-anyhex "\\{3\\}-" uuid-anyhex" \\{4\\}-" uuid-anyhex
+          "\\{12\\}\\b"))
+
+(defconst uuid-type3-re
+  (concat "\\b" uuid-anyhex "\\{8\\}-" uuid-anyhex "\\{4\\}-3"
+          uuid-anyhex "\\{3\\}-" uuid-anyhex "\\{4\\}-" uuid-anyhex
+          "\\{12\\}\\b"))
+
+(defconst uuid-type4-re
+  (concat "\\b" uuid-anyhex "\\{8\\}-" uuid-anyhex "\\{4\\}-4"
+          uuid-anyhex "\\{3\\}-[89ABab]" uuid-anyhex "\\{3\\}-" uuid-anyhex
+          "\\{12\\}\\b"))
+
+(defconst uuid-type5-re
+  (concat "\\b" uuid-anyhex "\\{8\\}-" uuid-anyhex "\\{4\\}-5"
+          uuid-anyhex "\\{3\\}-" uuid-anyhex "\\{4\\}-" uuid-anyhex
+          "\\{12\\}\\b"))
+
+(defconst uuid-empty-uuid "\\bd41d8cd9-8f00-3204-a980-0998ecf8427e\\b")
 
 (defgroup uuid nil "Highlighting of UUIDs"
   :group 'convenience)
 
-(defface uuid-face
+(defface uuid-type1-face
   '((t (:foreground "#2FADED")))
-  "Face for highlighting UUIDs"
+  "Face for highlighting Type 1 (timestamp) UUIDs"
   :group 'uuid)
 
-(defconst uuid-facedef
-  `((,uuid-re 0 '(face uuid-face
-                       uuid t
-                       keymap uuid-mode-map)
-              prepend)))
+(defface uuid-type1-empty-face
+  '((t (:foreground "#FF0000")))
+  "Face for highlighting empty Type 1 (MD5) UUIDs.
+
+This is the UUID of an empty input. It's displayed specially, this is
+often a sign of a problem with whatever generated it."
+  :group 'uuid)
+
+(defface uuid-type2-face
+  '((t (:foreground "#2FADED")))
+  "Face for highlighting Type 2 (DCE) UUIDs"
+  :group 'uuid)
+
+(defface uuid-type3-face
+  '((t (:foreground "#2FADED")))
+  "Face for highlighting Type 3 (MD5) UUIDs"
+  :group 'uuid)
+
+(defface uuid-type4-face
+  '((t (:foreground "#2FADED")))
+  "Face for highlighting Type 4 (random) UUIDs"
+  :group 'uuid)
+
+(defface uuid-type5-face
+  '((t (:foreground "#2FADED")))
+  "Face for highlighting Type 5 (SHA) UUIDs"
+  :group 'uuid)
+
+(defface uuid-unknown-type-face
+  '((t (:foreground "#FF0000")
+       (:slant 'italic)))
+  "Face for highlighting invalid UUIDs.
+
+   This is a UUID which looks valid, but lacks the internal structure
+   defined by RFC 4122."
+  :group 'uuid)
+
+(defconst uuid-font-lock-keywords
+  `((,uuid-type1-re 0 '(face uuid-type1-face
+                             uuid t
+                             keymap uuid-mode-map))
+    (,uuid-empty-uuid 0 '(face uuid-type1-empty-face
+                             uuid t
+                             keymap uuid-mode-map))
+    (,uuid-type2-re 0 '(face uuid-type1-face
+                             uuid t
+                             keymap uuid-mode-map))
+
+    (,uuid-type3-re 0 '(face uuid-type1-face
+                             uuid t
+                             keymap uuid-mode-map))
+    (,uuid-type4-re 0 '(face uuid-type1-face
+                             uuid t
+                             keymap uuid-mode-map))
+    (,uuid-type5-re 0 '(face uuid-type1-face
+                             uuid t
+                             keymap uuid-mode-map))
+    (,uuid-unknown-type-re 0 '(face uuid-unknown-type face
+                                    uuid t
+                                    keymap uuid-mode-map))))
 
 (defun uuid-save-uuid-at-point ()
   (interactive)
-  (save-match-data
-    (save-excursion
-      (unless (looking-at uuid-re)
-        (re-search-backwards uuid-re))
-      (kill-ring-save (match-beginning 0) (match-end 0)))))
+  (kill-new (thing-at-point 'uuid t)))
 
 (defconst uuid-mode-map
   (easy-mmode-define-keymap '(("w" . uuid-save-uuid-at-point))))
 
 (define-minor-mode uuid-mode "A mode for highlighting UUIDs"
-  nil " UUID" nil
+  nil nil nil
   (if uuid-mode
       (uuid-mode-enable)
     (uuid-mode-disable))
-    (font-lock-flush)
-    (font-lock-ensure))
+
+  (font-lock-fontify-buffer))
 
 (defun uuid-mode-enable ()
-  (font-lock-add-keywords nil uuid-facedef))
+  (font-lock-add-keywords nil uuid-font-lock-keywords))
 
 (defun uuid-mode-disable ()
-  (font-lock-remove-keywords nil uuid-facedef))
+  (font-lock-remove-keywords nil uuid-font-lock-keywords))
 
-(defun uuid-next-uuid (&optional n)
+(defun forward-uuid (&optional n)
   (interactive (list current-prefix-arg))
   (re-search-forward uuid-re nil t (or n 1)))
 
-(defun uuid-previous-uuid (&optional n)
+(defun backward-uuid (&optional n)
   (interactive (list current-prefix-arg))
   (re-search-backward uuid-re nil t (or n 1)))
+
+(put 'uuid 'bounds-of-thing-at-point
+     (lambda ()
+       (let ((thing (thing-at-point-looking-at
+                     uuid-re 500)))
+         (when thing
+           (cons (match-beginning 0) (match-end 0))))))
 
 (provide 'uuid)
 ;;; uuid.el ends here
